fe6e11d2702c715be005f4f73665c98a71c7383a
Khizer Amin <engrkhizeramin@gmail.com>
2018-08-28 11:51:46 +0200

    adding lib files to the repo

---
+++ spec/admin/login/login.controller.spec.js
@@ -0,0 +1,12 @@
+describe('The LoginController', function(){
+  'use strict';
+
+  beforeEach(function(){
+
+  })
+
+  it('LoginController', function(){
+    result = LoginController($state, LoginService, CurrentUserService)
+    expect(result).toBe();
+  });
+})
